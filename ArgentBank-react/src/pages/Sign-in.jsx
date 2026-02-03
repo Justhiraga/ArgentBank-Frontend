095fca6f8be1265768d5f4747e82b89b646c14e7
Patrick <abadie.patrick27@gmail.com>
2026-02-03 15:05:57 +0100

    navigate  fix

--- ArgentBank-react/src/pages/Sign-in.jsx
+++ ArgentBank-react/src/pages/Sign-in.jsx
@@ -1,33 +1,40 @@
-import { useNavigate } from 'react-router';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 import { authStatusSelector } from '../store/reducers/userReducer/userReducer.js';
-import Header from '../components/molecules/header.jsx'
-import Footer from '../components/molecules/footer.jsx'
-import Form from '../components/organismes/form.jsx'
-import './Sign-in.css'
+import Header from '../components/molecules/header.jsx';
+import Footer from '../components/molecules/footer.jsx';
+import Form from '../components/organismes/form.jsx';
+import './Sign-in.css';
 
 function SignIn() {
-  const navigate = (useNavigate);
+  const navigate = useNavigate();
   const isLoggedIn = useSelector(authStatusSelector);
-    if (!isLoggedIn) {
 
-      return (
-        <>
-            <title>Argent Bank - Sign In Page</title>
-            <Header />
-            <main className="main bg-dark">
-                <section className="sign-in-content">
-                    <i className="fa fa-user-circle"></i>
-                    <h1>Sign In</h1>
-                    <Form />
-                </section>
-            </main> 
-        <Footer />  
-        </>    
-      )
-    }else{
-      navigate('/user')
-    }  
-}   
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/user');
+    }
+  }, [isLoggedIn, navigate]);
 
-export default SignIn
+  if (!isLoggedIn) {
+    return (
+      <>
+        <title>Argent Bank - Sign In Page</title>
+        <Header />
+        <main className="main bg-dark">
+          <section className="sign-in-content">
+            <i className="fa fa-user-circle"></i>
+            <h1>Sign In</h1>
+            <Form />
+          </section>
+        </main>
+        <Footer />
+      </>
+    );
+  }
+
+  return null;
+}
+
+export default SignIn;
